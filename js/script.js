ca287f246d7aa2a06c9169253761928d5c8f7532
Caique Caleiro <caiquecaleiro@gmail.com>
2016-04-15 08:47:32 +0530

    Add OpenWeatherMap API

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-  console.log('document is ready');  
+  getLocalWeatherData();
+
+  function getLocalWeatherData() {
+    // Add a location API later
+    var lat = 57.5359;
+    var lon = 6.2263;
+    var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=metric';
+    $.getJSON(url, function(data) {
+      $('#location').text(data.name);
+      $('#temperature').text(data.main.temp);
+      $('#sky').text(data.weather[0].description);
+      $('#humidity').text(data.main.humidity);
+      console.log(data);
+    });
+  }
 });
