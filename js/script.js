0d1dd792b8b0c431e3a2d9a74143e7dd179ab821
Caique Caleiro <caiquecaleiro@gmail.com>
2016-04-15 09:19:21 +0530

    Add getLocation function

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
 $(document).ready(function() {
-  getLocalWeatherData();
+  getLocation();
 
-  function getLocalWeatherData() {
-    // Add a location API later
-    var lat = 57.5359;
-    var lon = 6.2263;
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var lat = position.coords.latitude;
+        var lon = position.coords.longitude;
+        getLocalWeatherData(lat, lon);
+      });
+    }
+  }
+
+  function getLocalWeatherData(lat, lon) {
     var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=metric';
     $.getJSON(url, function(data) {
       $('#location').text(data.name);
       $('#temperature').text(data.main.temp);
       $('#sky').text(data.weather[0].description);
-      $('#humidity').text(data.main.humidity);
+      $('#humidity').text(data.main.humidity + '%');
       console.log(data);
     });
   }
